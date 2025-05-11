622e9b2dc40457e6eb179d28c117723b0d60ab0a
bidri <zero.256dic+bi9ri.github.com@gmail.com>
2025-05-12 00:45:12 +0900

    add deploy workflow

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,8 +17,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'bi9dri', // Usually your GitHub org/user name.
+  projectName: 'emoklore-arknights-side-oripathy', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -27,8 +27,8 @@ const config: Config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'ja',
+    locales: ['ja'],
   },
 
   presets: [
